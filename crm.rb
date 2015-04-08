6a3ef16032cf6dfd514bf4b29edf88097a78ef6d
SVMBrown <scotb38@gmail.com>
2015-04-08 14:29:21 -0400

    Copied contact from other crm project and populated with fake data

--- crm.rb
+++ crm.rb
@@ -1,5 +1,21 @@
+require_relative 'contact'
 require 'sinatra'
 get '/' do
   @crm_app_name = "My CRM"
   erb :index
+end
+
+get '/contacts' do
+  @contacts = []
+  @contacts << Contact.new("Scot", "Brown", "sb@sb.com", "me")
+  @contacts << Contact.new("Cody", "Brown", "cb@cb.com", "bro")
+  @contacts << Contact.new("Chelsea", "Brown", "cb@cb.net", "Sis")
+  @contacts << Contact.new("Debi", "Brown", "db@db.net", "Mom")
+  @contacts << Contact.new("Dennis", "Brown", "db@db.com", "Dad")
+
+  erb :view_contacts
+end
+
+get '/contacts/new' do
+  "New Contact"
 end
