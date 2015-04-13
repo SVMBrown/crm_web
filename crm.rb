ef3e1a65d5c25e310a6207975193b220a39b0834
SVMBrown <scotb38@gmail.com>
2015-04-13 11:19:45 -0400

    Edit and delete functionality added

--- crm.rb
+++ crm.rb
@@ -14,7 +14,57 @@ get '/contacts/new' do
   erb :add_new_contact
 end
 
+get '/contacts/:id' do
+  if $rolodex.has_contact_with_id?(params[:id].to_i)
+    @contact = $rolodex.find_contact(params[:id].to_i)
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get "/contacts/:id/edit" do
+  if $rolodex.has_contact_with_id?(params[:id].to_i)
+    @contact = $rolodex.find_contact(params[:id].to_i)
+    erb :edit_contact
+  else
+    puts "something went wrong..."
+    raise Sinatra::NotFound
+  end
+end
+
+get '/contacts/:id/delete' do
+  if $rolodex.has_contact_with_id?(params[:id].to_i)
+    @contact = $rolodex.find_contact(params[:id].to_i)
+    erb :delete_contact
+  else
+    puts "something went wrong..."
+    raise Sinatra::NotFound
+  end
+end
+
 post '/contacts' do
   $rolodex.add_contact(params[:first_name].capitalize, params[:last_name].capitalize, params[:email], params[:note])
   redirect to('/contacts')
+end
+
+put '/contacts/:id' do
+  if $rolodex.has_contact_with_id?(params[:id].to_i)
+    $rolodex.modify(params[:id].to_i, 1, params[:first_name])
+    $rolodex.modify(params[:id].to_i, 2, params[:last_name])
+    $rolodex.modify(params[:id].to_i, 3, params[:email])
+    $rolodex.modify(params[:id].to_i, 4, params[:note])
+    redirect to("/contacts/#{params[:id].to_i}")
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  if $rolodex.has_contact_with_id?(params[:id].to_i)
+    $rolodex.remove_contact(params[:id].to_i)
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
 end
